b76b43aa162c3f577a3781f67b7809f53cd569be
soompy <sooper1137@gmail.com>
2020-06-11 17:44:44 +0900

    2020 카드고릴라 메인페이지 작업 완료

---
+++ js/gorilla.js
@@ -0,0 +1,19 @@
+$(document).on('click', 'a[href="#"]', function(e){
+    e.preventDefault();
+});
+
+function mMenu(){
+    document.getElementById('m_box').className += " active";
+}
+
+function Close(){
+    document.getElementById('m_box').className = document.getElementById('m_box').className.replace(" active", "");
+}
+
+function showSub(){
+    document.getElementById('subMenu').className += " on";
+}
+
+function hideSub(){
+    document.getElementById('subMenu').className = document.getElementById('subMenu').className.replace(" on", "");
+}
